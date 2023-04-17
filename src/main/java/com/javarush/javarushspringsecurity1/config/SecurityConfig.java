dd070a5057a78059c665b30970565ece8d1bb929
Andrey Mazikov <andymazik@gmail.com>
2023-04-17 20:58:55 +0300

    2. OAuth2 Authentication.

--- src/main/java/com/javarush/javarushspringsecurity1/config/SecurityConfig.java
+++ src/main/java/com/javarush/javarushspringsecurity1/config/SecurityConfig.java
@@ -27,14 +27,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                //.antMatchers("/**").hasRole("ADMIN")
-
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/guest").permitAll()
+        http.antMatcher("/**").authorizeRequests()
                 .antMatchers("/").permitAll()
-                .and().formLogin();
+                .anyRequest().authenticated()
+                .and().oauth2Login();
     }
 
     @Bean
